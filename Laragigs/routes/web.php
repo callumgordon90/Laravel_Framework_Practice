ca002596d396bc9ace3255ed644f824d8645895a
callumgordon90 <callumjosephiaingordon@gmail.com>
2023-02-19 14:19:26 +0100

    created some new endpoints in the web.php directory

--- Laragigs/routes/web.php
+++ Laragigs/routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//here 'view' refers to the blade folder: resources>views>welcome.blade.php
 Route::get('/', function () {
     return view('welcome');
+});
+
+//new endpoint practice with get request and response wrappers
+Route::get('/hello', function(){
+    return response("<h1>hello world!</h1>", 200)
+        ->header('Content-Type', 'text/plain');
+});
+
+
+//created another endpoint with the get method, but with the endpoint directory 'posts/{id}'
+Route::get('posts/{id}', function($id){
+    return response('Post ' . $id);
 });
