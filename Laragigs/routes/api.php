bb5464b1694745362fef93bb6bb20e025db84f5a
callumgordon90 <callumjosephiaingordon@gmail.com>
2023-02-19 15:20:17 +0100

    created a get enpoint to handle json body in array form in api.php!

--- Laragigs/routes/api.php
+++ Laragigs/routes/api.php
@@ -14,6 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//created a new get request endpoint which returns JSON in the body (for use with API);
+Route::get('/posts', function() {
+    return response()-> json([
+        'posts' => [
+                [
+                    'title' => 'Post One'
+                ]
+            ]
+        ]);
+});
+
+
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
