1de5ade37e2db63a17049b9e369f2e7a910bf6c4
callumgordon90 <callumjosephiaingordon@gmail.com>
2023-02-19 15:08:40 +0100

    created search endpoint in web.php

--- Laragigs/routes/web.php
+++ Laragigs/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,3 +31,8 @@ Route::get('/hello', function(){
 Route::get('posts/{id}', function($id){
     return response('Post ' . $id);
 })->where('id', '[0-9]+');
+
+Route::get('/search', function(Request $request) {
+    
+    dd($request->name . ' ' . $request->city);
+});
